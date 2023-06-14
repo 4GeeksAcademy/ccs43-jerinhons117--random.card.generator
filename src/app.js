3bd7fee6b8a85f0f655b1ff614de72ebd48544b0
jerinhons117 <131413483+jerinhons117@users.noreply.github.com>
2023-06-14 22:38:07 +0000

    card random generator, aun no se termina un 100%, pero logra el objetivo principal, falta lograr las otras funciones

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//simbolos de la carta
+function randomCard() {
+  let pok = ["diamond", "heart", "spades", "club"];
+  let indexPok = Math.floor(Math.random() * pok.length);
+
+  return pok[indexPok];
+}
+// numeros de la carta
+function randomNumber() {
+  let numerosPoker = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let indexNumber = Math.floor(Math.random() * numerosPoker.length);
+  return numerosPoker[indexNumber];
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const elementR = document.querySelector(".card").classList.add(randomCard());
+  document.querySelector(".card").innerHTML = randomNumber();
 };
